55152fa319b46347dc8ad2754aefd1d008dc80c9
vladimirivn <vladimiriv@hotmail.com>
2023-05-16 16:20:35 +0300

    Решение задачи к билету номер 12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        User user1 = new User("Vasya", 30);
+        User user2 = new User("Kolya", 23);
+        User user3 = new User("Vasya", 20);
+        User user4 = new User("Kolya", 23);
+        User user5 = new User("Kolya", 23);
+        User user6 = new User("Kolya", 27);
+        User user7 = new User("Olya", 25);
+
+        List<User> userList = Arrays.asList(user1, user2, user3, user4, user5, user6, user7);
+        System.out.println(getOldestUser(userList).toString());
+
     }
+
+    public static User getOldestUser(List<User> users) {
+        Set<User> uniqueUsers = new HashSet<>(users);
+        List<User> userList = new ArrayList<>(uniqueUsers);
+        userList.sort(Comparator.comparing(User::getAge).thenComparing(User::getName));
+        return userList.get(userList.size() - 1);
+    }
+
 }
