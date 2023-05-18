6e002b23b0f1f53bce96b73a0bec329c6f555f07
vladimirivn <vladimiriv@hotmail.com>
2023-05-18 16:36:47 +0300

    Решение задачи к билету номер 12

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,18 +11,37 @@ public class Main {
         User user4 = new User("Kolya", 23);
         User user5 = new User("Kolya", 23);
         User user6 = new User("Kolya", 27);
-        User user7 = new User("Olya", 25);
+        User user7 = new User("Olya", 30);
 
-        List<User> userList = Arrays.asList(user1, user2, user3, user4, user5, user6, user7);
-        System.out.println(getOldestUser(userList).toString());
+        List<User> userList = new ArrayList<>(Arrays.asList(user1, user2, user3, user4, user5, user6, user7));
+//        List<User> userList = Stream.of(user1, user2, user3, user4, user5, user6, user7).collect(Collectors.toList());
+        getFirstOldestUser(userList);
+        getAllOldestUser(userList);
 
     }
 
-    public static User getOldestUser(List<User> users) {
-        Set<User> uniqueUsers = new HashSet<>(users);
-        List<User> userList = new ArrayList<>(uniqueUsers);
+    public static void getFirstOldestUser(List<User> users) {
+        Set<User> userSet = new HashSet<>(users);
+        List<User> userList = new ArrayList<>(userSet);
         userList.sort(Comparator.comparing(User::getAge).thenComparing(User::getName));
-        return userList.get(userList.size() - 1);
+        System.out.println(userList.get(userList.size() - 1));
+    }
+
+    public static void getAllOldestUser(List<User> users) {
+        Set<User> userSet = new HashSet<>(users);
+        users.clear();
+        users.addAll(userSet);
+        users.sort(Comparator.comparing(User::getAge).thenComparing(User::getName));
+//        Collections.sort(users, Comparator.comparing(User::getAge).thenComparing(User::getName));
+        List<User> userList = new ArrayList<>();
+        int oldestAge = users.get(users.size() - 1).getAge();
+        for (User user : users) {
+            if (user.getAge() == oldestAge) {
+                userList.add(user);
+            }
+        }
+
+        System.out.println(userList);
     }
 
 }
